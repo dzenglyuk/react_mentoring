1373ab801f59e223ce4e9e1098aea3e9e6105d7e
Oleh Dzenhliuk <dzenglyuk23@ukr.net>
2020-09-01 17:52:26 +0300

    chunkArray function edited

--- src/components/lessons/Lesson3/Main.js
+++ src/components/lessons/Lesson3/Main.js
@@ -4,73 +4,76 @@ import PropTypes from 'prop-types';
 
 import './Main.css';
 
-const ourWithHelloWorld = WrappedComponent => {
-    return (props) => <WrappedComponent  {...props} hello={'World'} />
-}
+const ourWithHelloWorld = (WrappedComponent) => {
+  return (props) => <WrappedComponent {...props} hello={'World'} />;
+};
 
 const ComponentForWrap = ({ hello, ...props }) => {
-
-    return 'TEST COMPONENT HAS ' + hello;
-}
+  return 'TEST COMPONENT HAS ' + hello;
+};
 
 const WrappedComponentWithHello = ourWithHelloWorld(ComponentForWrap);
 
 //const ourConnect = (mapStateToProps, mapDispatchToProps) => WrappedComponent => props => <WrappedComponent {...props} {...mapStateToProps(store.getState())} {...mapDispatchToProps(store.dispatch)} />
 
-function chunkArray(myArray, chunk_size) {
-    const arrayLength = myArray.length;
-    const tempArray = [];
+function chunkArray(arr, cols) {
+	if (cols < 2) return [arr];
 
-    for (let index = 0; index < arrayLength; index += chunk_size) {
-        const myChunk = myArray.slice(index, index + chunk_size);
-        // Do something if you want with the group
-        tempArray.push(myChunk);
-    }
+	const length = arr.length,
+			isEven = length % cols === 0;
+	let result = [],
+		i = 0;
+	 
+	while (i < length) {
+			let size = isEven ? Math.floor(length / cols) : Math.ceil((length - i) / cols--);
+			result.push(arr.slice(i, (i += size)));
+	}
 
-    return tempArray;
+	return result;
 }
 
 const ColumnSplitter = React.memo(({ children, itemsInColumns }) => {
-    const columns = useMemo(() => chunkArray(React.Children.toArray(children), itemsInColumns), [children, itemsInColumns]);
+  const columns = useMemo(
+    () => chunkArray(React.Children.toArray(children), itemsInColumns),
+    [children, itemsInColumns]
+  );
 
-    return <div className={'columns-container'}>
-        <div className={'rowcheg'}>
-            {columns.map(column => <div className={'columncheg'}>{column}</div>)}
-        </div>
+  return (
+    <div className={'columns-container'}>
+      <div className={'rowcheg'}>
+        {columns.map((column, index) => (
+          <div className={'columncheg'}>{column}</div>
+        ))}
+      </div>
     </div>
-})
+  );
+});
 
-const Aka47Filters = ({ filters, columnsCount }) => {
-    const count = Math.ceil(filters.length / columnsCount);
-
-    return <ColumnSplitter itemsInColumns={count}>
-        {filters}
-    </ColumnSplitter>;
-}
-
-Aka47Filters.defaultProps = {
-    columnsCount: 2
-}
+ColumnSplitter.defaultProps = {
+  itemsInColumns: 2,
+};
 
 const columns = [
-    <div>1st</div>,
-    <div>2nd</div>,
-    <div>3rd</div>,
-    <div>4th</div>,
-    <div>5th</div>,
-    <div>6th</div>,
-    <div>7th</div>,
-    <div>8th</div>,
-    <div>9th</div>,
-    <div>10th</div>,
-    <div>11th</div>,
-]
+  <div>1st</div>,
+  <div>2nd</div>,
+  <div>3rd</div>,
+  <div>4th</div>,
+  <div>5th</div>,
+  <div>6th</div>,
+  <div>7th</div>,
+  <div>8th</div>,
+  <div>9th</div>,
+  <div>10th</div>,
+  <div>11th</div>,
+];
 
 const Lesson3 = () => {
-    return <React.Fragment>
-        <WrappedComponentWithHello />
-        <Aka47Filters columnsCount={5} filters={columns} />
+  return (
+    <React.Fragment>
+      <WrappedComponentWithHello />
+			<ColumnSplitter itemsInColumns={5}>{columns}</ColumnSplitter>
     </React.Fragment>
-}
+  );
+};
 
 export default Lesson3;
